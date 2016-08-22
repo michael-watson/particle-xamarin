27a6332c5f005bb88cfdb17c8e1311b617039f83
Michael Watson <michaelw@michaelw-mac.local>
2016-08-22 13:40:09 -0700

    initial upload

---
+++ samples/My Devices/Droid/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace MyDevices.Droid
+{
+	[Activity(Label = "MyDevices.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            var density = Resources.DisplayMetrics.Density;
+
+            App.ScreenWidth = (double)(Resources.DisplayMetrics.WidthPixels - 0.5f) / density;
+            App.ScreenHeight = (double)(Resources.DisplayMetrics.HeightPixels - 0.5f) / density;
+
+            Console.WriteLine($"Density: {density}");
+
+            LoadApplication(new App());
+		}
+	}
+}
---
+++ samples/My Devices/MyDevices/Pages/LoginPage.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Reactive.Linq;
+
+using Xamarin.Forms;
+using System.Collections.Generic;
+using Akavache;
+using Particle;
+using Particle.Models;
+using MyDevices.ViewModels;
+
+using MyLoginUI.Pages;
+
+namespace MyDevices.Pages
+{
+	public class LoginPage : ReusableLoginPage
+	{
+		bool isInitialized = false;
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			if (!isInitialized)
+			{
+				Navigation.InsertPageBefore(new MyDevicesPage(), this);
+				isInitialized = true;
+			}
+		}
+
+		public override async void Login(string userName, string passWord, bool saveUserName)
+		{
+			base.Login(userName, passWord, saveUserName);
+
+			await ParticleCloud.SharedInstance.CreateOAuthClientAsync(App.Token, "xamarin");
+			var response = await ParticleCloud.SharedInstance.LoginWithUserAsync(userName, passWord);
+
+			if (ParticleCloud.AccessToken != null && response)
+			{
+				await BlobCache.UserAccount.InsertObject("Access Token", ParticleCloud.AccessToken.Token);
+				await BlobCache.UserAccount.InsertObject("Refresh Token", ParticleCloud.AccessToken.RefreshToken);
+				await BlobCache.UserAccount.InsertObject("Expiration", ParticleCloud.AccessToken.Expiration);
+				await BlobCache.UserAccount.InsertObject("Username", userName);
+				App.HasValidToken = true;
+
+				await Navigation.PopAsync();
+			}
+			else
+				await DisplayAlert("Login Error", "Invalid Login", "Try Again");
+		}
+
+		public override async void RunAfterAnimation()
+		{
+			base.RunAfterAnimation();
+
+			try
+			{
+				var username = await BlobCache.UserAccount.GetObject<string>("Username");
+				SetUsernameEntry(username);
+
+				var accessToken = await BlobCache.UserAccount.GetObject<string>("Access Token");
+				var refreshToken = await BlobCache.UserAccount.GetObject<string>("Refresh Token");
+				var expiration = await BlobCache.UserAccount.GetObject<DateTime>("Expiration");
+
+				//if (expiration > DateTime.Now && accessToken != null)
+				//{
+				//	ParticleCloud.AccessToken = new ParticleAccessToken(accessToken, refreshToken, expiration);
+				//	await Navigation.PopAsync(false);
+				//}
+			}
+			catch (Exception e)
+			{
+				System.Diagnostics.Debug.WriteLine(e.Message);
+			}
+		}
+	}
+}
